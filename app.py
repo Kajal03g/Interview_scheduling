45b4c31635b068ce6b42108faa86935e5f120017
Kajal03g <120003423+Kajal03g@users.noreply.github.com>
2023-09-06 15:13:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'kajal03g@gmail.com'  # Replace with your Gmail email address
 app.config['MAIL_PASSWORD'] = 'iewrwnyvngkbgwmg'  # Replace with your Gmail password
-app.config['MAIL_DEFAULT_SENDER'] = 'kajal03g@gmail.com'  # Replace with your Gmail email address
+app.config['MAIL_DEFAULT_SENDER'] = 'kajal03g@gmail.com'  # Replace with your Gmail address
 
 mail = Mail(app)
 
